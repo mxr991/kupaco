7d2b8754b417eba4673f3261be91795c52ca6046
mxr991 <erika.davis@go.shoreline.edu>
2025-03-02 07:30:47 +0300

    y

---
+++ Content/general/js/lang.js
@@ -0,0 +1,14 @@
+﻿const en = {
+  lblLanguage: "ع",
+  lblProducts: "Products",
+};
+
+const ar = {
+  lblLanguage: "E",
+  lblProducts: "منتجات",
+};
+
+const appLang = {
+  en: { ...en },
+  ar: { ...ar },
+};
---
+++ Scripts/appsettings.js
@@ -0,0 +1,22 @@
+﻿
+const host = window.location.protocol + "//" + window.location.host;
+const appBaseUrl = "file:///Users/mohamedhorani/websites/orderkupco.net/"
+
+
+
+function getCookieByName(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+// disable right click
